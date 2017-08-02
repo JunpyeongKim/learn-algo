6a5f46008f4a1f2d820edce031cfadd7573ffb21
Junpyeong Kim <junpyeong.kim@gmail.com>
2017-08-02 13:36:41 +0900

    [develop] treegraph04

--- cc-java/src/ch04treegraph/TreeGraph04.java
+++ cc-java/src/ch04treegraph/TreeGraph04.java
@@ -25,82 +25,19 @@ import java.util.LinkedList;
  *                     #135. You should be able to come up with an algorithm involving both depth-first search and breadth-first search.
  */
 
-/**
- * BFS, DFS 2가지 방법으로 풀 수 있다.
- * DFS 방법으로는 BFS 의 Traversal 방법과 비슷한 pre-order Traversal 로 풀어본다. --> 단, 반복되는 패턴을 함수로 만든 recursion 에서는 각 노드가 어느 레벨인지 표시 필요
- *  --> TreeNode 레퍼런스가 변하는 변수이면 null 체크는 반드시 필요하다. --> 가장 간단한 노드 3개의 트리 고려후 --> leaf node 처리 순서로 고민하면 된다.
- */
 public class TreeGraph04 {
-    /*
-    // BSF
-    public static ArrayList<LinkedList<TreeNode>> createLevelLinkedList(TreeNode root) {
-        ArrayList<LinkedList<TreeNode>> result = new ArrayList<LinkedList<TreeNode>>();
-
-        LinkedList<TreeNode> current = new LinkedList<TreeNode>();
-        if (current != null) {
-            current.add(root);
-        }
-
-        while (current.size() > 0) {
-            result.add(current);
-
-            LinkedList<TreeNode> parents = current;
-            current = new LinkedList<TreeNode>();
-            for (TreeNode parent : parents) {
-                if (parent.getLeft() != null)
-                    current.add(parent.getLeft());
-
-                if (parent.getRight() != null)
-                    current.add(parent.getRight());
-            }
-        }
-
-        return result;
-    }
-
-    // DFS
-
-
-    public static void printResult(ArrayList<LinkedList<TreeNode>> result) {
-
-    }
-
-    public static void main(Strins[] args) {
-        int[] nodes_flattened = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
-        TreeNode root = AssortedMethods.createTreeFromArray(nodes_flattened);
-
-        ArrayList<LinkedList<TreeNode>> list = createLevelLinkedList(root);
-        printResult(list);
-    }
-    */
-
-    public static void printResult(ArrayList<LinkedList<TreeNode>> result) {
-        int depth = 0;
-        for (LinkedList<TreeNode> list : result) {
-            System.out.print("Linked list at depth " + depth + ":");
-
-            // Iterator<TreeNode> iterator = list.listIterator();
-            // iterator.hasNext() --> (TreeNode)iterator.next()
-            for(TreeNode t : list) {
-                System.out.println(" " + t.data);
-            }
-            System.out.println();
-
-            depth++;
-        }
-    }
-
-    // BFS: Level-order traversal
-    // LinkedList가 레벨별로 존재하므로, 즉 부모 버퍼가 존재하므로 Queue 를 사용할 필요가 없다.
-    // 즉, 부모 버퍼로서 Queue 대신 이미 존재하는 레벨별 LinkdedList 를 사용하면 된다
-    public static ArrayList<LinkedList<TreeNode>> createLevelLinkedList01(TreeNode root) {
+    //--------------------------------------------------------------------------------
+    // Solution #1. Breadth First Search (a.k.a Level-order traversal)
+    //--------------------------------------------------------------------------------
+    public static ArrayList<LinkedList<TreeNode>> createDepthLinkedListBFS(TreeNode root) {
         ArrayList<LinkedList<TreeNode>> result = new ArrayList<LinkedList<TreeNode>>();
 
         if (root != null) {
-            // visit
+            // Visit a root
             LinkedList<TreeNode> current = new LinkedList<TreeNode>();
             current.add(root);
 
+            // Queue 대신 이미 존재하는 Depth별 LinkedList 를 사용하면 된다.
             while (current.size() > 0) {
                 result.add(current);
 
@@ -122,48 +59,80 @@ public class TreeGraph04 {
         return result;
     }
 
-    // DFS: pre-order traversal --> DLR
-    private static void createLevelLinkedList02(TreeNode node, int level, ArrayList<LinkedList<TreeNode>> result) {
+
+    //--------------------------------------------------------------------------------
+    // Solution #2. Depth First Search (a.k.a pre-order traversal; DLR) --> "Recurse"
+    //--------------------------------------------------------------------------------
+    private static void createDepthLinkedListDFS(TreeNode node,
+                                                 int depth,
+                                                 ArrayList<LinkedList<TreeNode>> result) {
         if (node == null) {
             return;
         }
 
         LinkedList<TreeNode> current;
 
-        if (result.size() == level) {   // or null == result.get(level)
+        if (result.size() == depth) {   // or null == result.get(depth)
             current = new LinkedList<TreeNode>();
             result.add(current);
         } else {
-            current = result.get(level);
+            current = result.get(depth);
         }
 
         current.add(node);
-        createLevelLinkedList02(node.left, level + 1, result);
-        createLevelLinkedList02(node.right, level + 1, result);
+        createDepthLinkedListDFS(node.left, depth + 1, result);
+        createDepthLinkedListDFS(node.right, depth + 1, result);
     }
 
-    public static ArrayList<LinkedList<TreeNode>> createLevelLinkedList02(TreeNode root) {
+    public static ArrayList<LinkedList<TreeNode>> createDepthLinkedListDFS(TreeNode root) {
         ArrayList<LinkedList<TreeNode>> result = new ArrayList<LinkedList<TreeNode>>();
 
         if (root != null) {
-            createLevelLinkedList02(root, 0, result);
+            createDepthLinkedListDFS(root, 0, result);
         }
 
         return result;
     }
 
 
+    //--------------------------------------------------------------------------------
     // Main
+    //--------------------------------------------------------------------------------
+    private static void printResult(ArrayList<LinkedList<TreeNode>> result) {
+        int depth = 0;
+
+        for (LinkedList<TreeNode> list : result) {
+            System.out.print("Linked list at depth " + depth + ":");
+
+            /* Alternative 01
+            Iterator<TreeNode> iterator = list.listIterator();
+            while (iterator.hasNext()) {
+                System.out.print(" " + iterator.next().data);
+            }
+            */
+
+            // Alternative 02
+            for(TreeNode t : list) {
+                System.out.print(" " + t.data);
+            }
+
+            System.out.println();
+            depth++;
+        }
+    }
+
     public static void main(String[] args) {
         int[] nodes_flattened = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-        TreeNode root = AssortedMethods.createTreeFromArray(nodes_flattened);
+        ArrayList<LinkedList<TreeNode>> list;
 
         // BFS
-        ArrayList<LinkedList<TreeNode>> list = createLevelLinkedList01(root);
+        TreeNode node1 = AssortedMethods.createTreeFromArray(nodes_flattened);
+        list = createDepthLinkedListBFS(node1);
+        printResult(list);
 
         // DFS
-        // ArrayList<LinkedList<TreeNode>> list = createLevelLinkedList02(root);
-
+        TreeNode node2 = AssortedMethods.createTreeFromArray(nodes_flattened);
+        list = createDepthLinkedListDFS(node2);
         printResult(list);
     }
 }
