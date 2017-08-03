e83e01b6031be54f7e035d2f90c419d888f5ef21
Junpyeong Kim <junpyeong.kim@gmail.com>
2017-08-03 13:47:43 +0900

    [develop] prepare for treegraph 05/08

--- cc-java/src/ch04treegraph/TreeGraph.java
+++ cc-java/src/ch04treegraph/TreeGraph.java
@@ -94,7 +94,82 @@ package ch04treegraph;
  *                     #135. You should be able to come up with an algorithm involving both depth-first search and breadth-first search.
  */
 
-// WIP 7
+// WIP 5
+/**
+ * 4.5 어떤 이진 트리가 이진 탐색 트리인지 판별하는 함수를 구현하라.
+ *
+ * (6E)
+ * 4.5 Validate BST: Implement a function to check if a binary tree is a binary search tree.
+ *
+ *                   Hints:
+ *                   #35. If you traversed the tree using an in-order traversal and the elements were truly in the right order,
+ *                        does this indicate that the tree is actually in order?
+ *                        What happens for duplicate elements?
+ *                        If duplicate elements are allowed, they must be on a specific side (usually the left).
+ *                   #57. To be a binary search tree, it's not sufficient that the left.value <= current.value < right.value for each node.
+ *                        Every node on the left must be less than the current node, which must be less than all the nodes on the right.
+ *                   #86. If every node on the left must be less than or equal to the current node,
+ *                        then this is really the same thing as saying that the biggest node on the left must be less than or equal to the current node.
+ *                   #113. Rather than validating the current node's value against leftTree.max and rightTree.min, can we flip around the logic?
+ *                         Validate the left tree's nodes to ensure that they are smaller than current.value.
+ *                   #128. Think about the checkBST function as a recursive function that ensures each node is within an allowable (min, max) range.
+ *                         At first, this range is infinite.
+ *                         When we traverse to the left, the min is negative infinity and the max is root.value.
+ *                         Can you implement this recursive function and properly adjust these ranges as you traverse the tree?
+ */
+
+// WIP 8
+/**
+ * 4.8 두 개의 큰 이진 트리 T1, T2가 있다고 하자.
+ *     T1에는 수백만 개의 노드가 있고, T2에는 수백 개 정도의 노드가 있다.
+ *     T2가 T1의 하위 트리인지 판별하는 알고리즘을 만들라.
+ *
+ *     T1 안에 노드 n이 있어 그 노드의 하위 트리가 T2와 동일하면, T2는 T1의 하위 트리다.
+ *     다시 말해, T1에서 n부터 시작하여 그 아래쪽을 끊어내면, 그 결과가 T2와 동일해야 한다.
+ *
+ * (4E)
+ * 4.7 You have two very large binary trees: T1, with millions of nodes, and T2, with hundreds of nodes.
+ *     Create an algorithm to decide if T2 is a subtree of T1.
+ *
+ * (6E)
+ * 4.10 Check Subtree: T1 and T2 are two very large binary trees, with T1 much bigger than T2.
+ *                     Create an algorithm to determine if T2 is a subtree of T1.
+ *
+ *                     A tree T2 is a subtree of T1 if there exists a node n in T1 such that the subtree of n is identical to T2 .
+ *                     That is, if you cut off the tree at node n, the two trees would be identical.
+ *
+ *                     Hints:
+ *                     #4. If T2 is a subtree of T1, how will its in-order traversal compare to T1's?
+ *                         What about its pre-order and post-order traversal?
+ *                     #11. The in-order traversals won't tell us much.
+ *                          After all, every binary search tree with the same values (regardless of structure) will have the same in-order traversal.
+ *                          This is what in-order traversal means: contents are in-order.
+ *                          (And if it won't work in the specific case of a binary search tree, then it certainly won't work for a general binary tree.)
+ *                          The preorder traversal, however, is much more indicative.
+ *                     #18. You may have concluded that if T2.preorderTraversal() is a substring of T1.preorderTraversal (), then T2 is a subtree of T1.
+ *                          This is almost true, except that the trees could have duplicate values.
+ *                          Suppose T1 and T2 have all duplicate values but different structures. The pre-order traversals will look the same
+ *                          even though T2 is not a subtree of T1.
+ *                          How can you handle situations like this?
+ *                     #31. Although the problem seems like it stems from duplicate values, it's really deeper than that.
+ *                          The issue is that the pre-order traversal is the same only because there are null nodes that we skipped over (because they're null ).
+ *                          Consider inserting a placeholder value into the pre-order traversal string whenever you reach a null node.
+ *                          Register the null node as a "real " node so that you can distinguish between the different structures.
+ *                     #37. Alternatively, we can handle this problem recursively.
+ *                          Given a specific node within T1, can we check to see if its subtree matches T2?
+ */
+
+
+//--------------------------------------------------------------------------------
+// Korean Edition: Unsolved --> 6, 7, 9
+//--------------------------------------------------------------------------------
+// TBD 6
+/**
+ * 4.6 정순회 기준으로, 이진 탐색 트리 내의 한 노드가 주어지면 그 노드의 '다음' 노드를 찾아내는 알고리즘을 작성하라.
+ *     각 노드에는 부모 노드를 가리키는 링크가 존재한다고 가정한다.
+ */
+
+// TBD 7
 /**
  * 4.7 이진 트리 내의 두 노드의 공통 선조(ancestor) 노드를 찾는 알고리즘을 설계하고 구현하라.
  *     자료구조 내에 부가적인 노드를 저장해 두는 일은 금한다.
@@ -116,44 +191,6 @@ package ch04treegraph;
  *                            Hints: # 10, #16, #28, #36, #46, #70, #80, #96
  */
 
-// Done 8
-/**
- * 4.8 두 개의 큰 이진 트리 T1, T2가 있다고 하자.
- *     T1에는 수백만 개의 노드가 있고, T2에는 수백 개 정도의 노드가 있다.
- *     T2가 T1의 하위 트리인지 판별하는 알고리즘을 만들라.
- *
- *     T1 안에 노드 n이 있어 그 노드의 하위 트리가 T2와 동일하면, T2는 T1의 하위 트리다.
- *     다시 말해, T1에서 n부터 시작하여 그 아래쪽을 끊어내면, 그 결과가 T2와 동일해야 한다.
- *
- * (4E)
- * 4.7 You have two very large binary trees: T1, with millions of nodes, and T2, with hundreds of nodes.
- *     Create an algorithm to decide if T2 is a subtree of T1.
- *
- * (6E)
- * 4.10 Check Subtree: T1 and T2 are two very large binary trees, with T1 much bigger than T2.
- *                     Create an algorithm to determine if T2 is a subtree of T1.
- *
- *                     A tree T2 is a subtree of T1 if there exists a node n in T1 such that the subtree of n is identical to T2 .
- *                     That is, if you cut off the tree at node n, the two trees would be identical.
- *
- *                     Hints: #4, #11, #18, #31, #37
- */
-
-
-//--------------------------------------------------------------------------------
-// Korean Edition: Unsolved --> 5, 6, 9
-//--------------------------------------------------------------------------------
-// TBD 5
-/**
- * 4.5 어떤 이진 트리가 이진 탐색 트리인지 판별하는 함수를 구현하라.
- */
-
-// TBD 6
-/**
- * 4.6 정순회 기준으로, 이진 탐색 트리 내의 한 노드가 주어지면 그 노드의 '다음' 노드를 찾아내는 알고리즘을 작성하라.
- *     각 노드에는 부모 노드를 가리키는 링크가 존재한다고 가정한다.
- */
-
 // TDB 9
 /**
  * 4.9 각 노드에 어떤 값이 저장되어 있는 이진 트리 하나와 값 n이 주어졌을 때, n과 같은 값을 갖는 모든 경로를 찾아라.
@@ -162,8 +199,6 @@ package ch04treegraph;
 
 
 //--------------------------------------------------------------------------------
-// English Edition: Unsolved --> 4E(5, 6,7, 8), 6E(5, 6, 7, 8, 9, 11, 12)
+// English Edition: Unsolved --> 4E(5, 6,8), 6E(6, 7, 8, 9, 11, 12)
 //--------------------------------------------------------------------------------
-
-public class TreeGraph {
-}
+public class TreeGraph {}
