4117d95ee9c235df70b130e35dfd73ef00d4793b
Junpyeong Kim <junpyeong.kim@gmail.com>
2017-08-05 22:51:45 +0900

    [develop] modify each READ.md and script files.

---
+++ data_structures_and_algorithms_using_javascript.sh
@@ -0,0 +1,4 @@
+echo ====================================================================
+echo git clone https://github.com/oreillymedia/data_structures_and_algorithms_using_javascript.git
+echo ====================================================================
+git clone https://github.com/oreillymedia/data_structures_and_algorithms_using_javascript.git
