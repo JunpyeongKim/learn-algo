95a8f05bedb24624ab7166f4bce2cefa7dfa4861
Junpyeong Kim <junpyeong.kim@gmail.com>
2017-10-16 14:17:20 +0900

    [cc-java] ArrayString06

--- cc-java/src/ch01arraystring/ArrayString06.java
+++ cc-java/src/ch01arraystring/ArrayString06.java
@@ -2,7 +2,7 @@ package ch01arraystring;
 
 /**
  * 1.6 이미지를 표현하는 NxN 행렬이 있다. 이미지의 각 픽셀은 4바이트로 표현된다.
- *     이때 이미지를 90도 회전시키는 메서드를 작성하라.
+ *     이때, 이미지를 90도 회전시키는 메서드를 작성하라.
  *     부가적인 행렬을 사용하지 않고서도 할 수 있겠는가?
  *
  * (4E)
@@ -11,11 +11,18 @@ package ch01arraystring;
  *     Can you do this in place?
  *
  * (6E)
- * 1.7 Rotate Matrix:
+ * 1.7 Rotate Matrix: Given an image represented by an NxN matrix, where each pixel in the image is 4 bytes,
+ *                    write a method to rotate the image by 90 degrees.
+ *                    Can you do this in place?
  *
+ *                    Hints: #51, #100
  */
-public class Question06TBD {
+public class ArrayString06 {
     public static void rotate(int[][] matrix, int n) {
         // TODO:
+    }
+
+    public static void main(String[] args) {
+
     }
 }
