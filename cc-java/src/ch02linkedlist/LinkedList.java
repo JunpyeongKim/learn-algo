12dea84eb938de6c829869c076909c58e786cfa4
Junpyeong Kim <junpyeong.kim@gmail.com>
2017-10-18 15:36:36 +0900

    [cc-java] LinkedList03: remark

--- cc-java/src/ch02linkedlist/LinkedList.java
+++ cc-java/src/ch02linkedlist/LinkedList.java
@@ -24,30 +24,30 @@ package ch02linkedlist;
 
 // Done 3
 /**
- * 2.3 단방향 연결 리스트의 중간에 있는 노드 하나를 삭제하는 알고리즘을 구현하라.
- *     삭제할 노드에 대한 접근만 가능하다는 것에 유의하라가
+ * 2.3 단방향 연결리스트 중간에 있는 노드 하나를 삭제하는 알고리즘을 구현하라.
+ *     삭제할 노드에 대한 접근만 가능하다는 것에 유의하자.
  *
  *     - 예
- *          입력: 연결리스트 a->b->c->d->e 의 노드 c
- *          출력: 아무것도 반환할 필요 없고, 결과로 연결리스트가 a->b->d->e 가 되어 있으면 OK
+ *          입력: 연결 리스트 a->b->c->d->e 의 노드 c
+ *          출력: 아무것도 반환할 필요없고, 결과로 연결리스트가 a->b->d->e 가 되어 있으면 OK
  *
  * (4E)
- * 2.3 Implement an algorithm to delete a node in the middle of a single linked list,
- *     given only access to that node.
+ * 2.3 Implement an algorithm to delete a node in the middle of a singly linked list, given only access to that node.
  *
- *     EXAMPLE
- *      Input: the node ‘c’ from the linked list a->b->c->d->e
- *      Result: nothing is returned, but the new linked list looks like a->b->d->e
+ *      EXAMPLE:
+ *          Input: the node ‘c’ from the linked list a->b->c->d->e
+ *          Result: nothing is returned, but the new linked list looks like a->b->d->e
  *
  * (6E)
- * 2.3 Delete Middle Node: Implement an algorithm to delete a node in the middle (i.e., any node but the first and last node,
- *                         not necessarily the exact middle) of a singly linked list, given only access to that node.
+ * 2.3 Delete Middle Node: Implement an algorithm to delete a node in the middle
+ *                         (i.e., any node but the first and last node, not necessarily the exact middle)
+ *                         of a singly linked list, given only access to that node.
  *
- *                         EXAMPLE
- *                          Input: the node c from the linked list a->b->c->d->e->f
- *                          Result: nothing is returned, but the new linked list looks like a->b->d->e->f
+ *      EXAMPLE:
+ *          Input: the node ‘c’ from the linked list a->b->c->d->e->f
+ *          Result: nothing is returned, but the new linked list looks like a->b->d->e->f
  *
- *                         Hints: #72
+ *      Hint #72.
  */
 
 // Done 4
--- cc-java/src/ch02linkedlist/LinkedList03.java
+++ cc-java/src/ch02linkedlist/LinkedList03.java
@@ -12,8 +12,7 @@ import lib.LinkedListNode;
  *          출력: 아무것도 반환할 필요없고, 결과로 연결리스트가 a->b->d->e 가 되어 있으면 OK
  *
  * (4E)
- * 2.3 Implement an algorithm to delete a node in the middle (Le., any node but the first and last node,
- *      not necessarily the exact middle) of a singly linked list, given only access to that node.
+ * 2.3 Implement an algorithm to delete a node in the middle of a singly linked list, given only access to that node.
  *
  *      EXAMPLE:
  *          Input: the node ‘c’ from the linked list a->b->c->d->e
